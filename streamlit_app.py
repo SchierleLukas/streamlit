36efa094158b47bcdbbdac85de7568309729fd0d
Lukas Schierle <schierle.lukas@gmail.com>
2023-09-26 21:16:27 +0200

    test: auto deployment of streamlit cloud app

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,7 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+# Example 6
+
+st.write('This is a minor change, to test if the streamlit app is automatically beeing deployed.')
